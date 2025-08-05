67608e7f93a8967680c4a79a8911ca7014e03f0e
LTasaycoQ☕🐋 <luistasayco3030@gmail.com>
2025-08-05 00:36:16 +0000

    jar actualizacion

--- src/test/java/pe/edu/vallegrande/videosdonwload/VideosDonwloadApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/videosdonwload/VideosDonwloadApplicationTests.java
@@ -1,13 +1,8 @@
 package pe.edu.vallegrande.videosdonwload;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VideosDonwloadApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
